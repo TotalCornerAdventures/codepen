93896115430eb60bfa3ef7fc168aed13e01f6c38
Conor <conor@murray-playfair.com>
2025-05-29 22:18:40 +0100

    Initial commit

---
+++ codepen/index.ts
@@ -0,0 +1,10 @@
+// A simple TypeScript program that adds two numbers and prints the result
+
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+const result = add(2, 3);
+console.log("The sum is:", result);
+
+add(5, 7);
